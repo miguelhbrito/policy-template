528f8e6cd7e0900ed1e250694abd9751d94fc10a
Miguel Henrique De Brito Pereira <miguelhdbpereira@MacBook-Air-de-Miguel.local>
2023-04-14 19:07:22 -0400

    fix: lint

--- validate.go
+++ validate.go
@@ -44,7 +44,7 @@ func validate(payload []byte) ([]byte, error) {
 	logger.Debug("validating label keys")
 
 	// Validates every label key to check if is a palindrome
-	for key, _ := range pod.Metadata.Labels {
+	for key := range pod.Metadata.Labels {
 		if settings.IsPalindrome(key) {
 			logger.InfoWithFields("rejecting label key", func(e onelog.Entry) {
 				e.String("label key", key)
